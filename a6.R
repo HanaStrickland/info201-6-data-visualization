60fe94886d1b01d7fa92cca8ca10b0734610eb18
hanastrickland <strickh@uw.edu>
2018-05-06 17:11:18 -0700

    Add interactive plot

--- a6.R
+++ a6.R
@@ -81,7 +81,7 @@ get_gdp_co2_year_graphic <- function() {
   # Plot data
   plot_graphic <- ggplot(data = both_co2_gdp) +
     geom_point(mapping = aes(x = year, y = total_co2_emissions_kt, color = total_gdp)) +
-    scale_color_gradientn(colors = c("red", "green")) +
+    scale_color_gradientn(colors = c("blue", "orange")) +
     theme(axis.text.x = element_text(angle = 60, hjust = 1)) +
     labs(title = "Change in CO2 Emissions Over Time in Relation to GDP")
 
@@ -147,19 +147,20 @@ get_world_map <- function() {
     labels = c("less than 100kt", "100 - 1,000kt", "1,000 - 10,000kt", "10,000 - 100,000kt", "100,000 - 1,000,000kt", "1,000,000 - 10,000,000"),
     ordered_result = TRUE
   )
+  
+  factors_of_emissions <- as.data.frame(factors_of_emissions) #ordered factor to data frame
 
-
-  factors_of_emissions <- as.data.frame(factors_of_emissions)
-
-  world_map_and_co2$emission_level <- factors_of_emissions
+  world_map_and_co2$emission_level <- factors_of_emissions # new column for emissions level
 
   plot_graphic <- ggplot(data = world_map_and_co2) +
     geom_polygon(aes(x = long, y = lat, group = group, fill = emission_level)) +
     coord_quickmap() +
     scale_fill_brewer(type = div, palette = "Set2") +
-    theme(legend.position = "bottom") 
+    theme(legend.position = "bottom") + 
+    labs(title = "World Map of CO2 Emissions", 
+         caption = "(Based on data from the World Bank)",
+         x = "Longitude", y = "Latitude")
     
-
   
   plot_graphic
 }
@@ -177,57 +178,96 @@ get_world_map()
 
 ### Interactive Visualization ###
 
+# GDP and CO2 in 2014 
+
 # Filter for co2 emissions and gdp
-filter_co2_emissions_kt()
+co2_interactive <- filter_co2_emissions_kt() %>% 
+  select(-Series.Code)  
 
-filter_gdp()
+gdp_interactive <- filter_gdp() %>% 
+  select(-Series.Code, -Series.Name)
 
-# sum columns of data by year
-total_co2_emissions_kt <- colSums(filter_co2_emissions_kt()[, -1:-2], na.rm = TRUE)
-total_gdp <- colSums(filter_gdp()[, -1:-4], na.rm = TRUE)
+# Change year names on gdp data frame
+colnames(gdp_interactive) <- c("Country.Name", "Country.Code", 
+                               "YR1998", "YR1999", "YR2000", "YR2001", "YR2002", "YR2003", 
+                               "YR2004", "YR2005", "YR2006", "YR2007", 
+                               "YR2008", "YR2009", "YR2010", "YR2011",
+                                       "YR2012", "YR2013", "YR2014")
+# make them long form
+co2_long <- gather(co2_interactive,
+                        key = year, value = co2_emitted, 
+                   "YR1998", "YR1999", "YR2000", "YR2001", "YR2002", "YR2003",
+                   "YR2004", "YR2005", "YR2006", "YR2007",
+                   "YR2008", "YR2009", "YR2010", "YR2011",
+                   "YR2012", "YR2013", "YR2014")
 
-# Make summed info into dataframes
-total_co2_df <- as.data.frame(total_co2_emissions_kt)
-total_gdp_df <- as.data.frame(total_gdp)
-
-# change year row name into "year"
-total_co2_df <- total_co2_df %>%
-  mutate(year = rownames(total_co2_df))
-total_gdp_df <- total_gdp_df %>%
-  mutate(year = rownames(total_gdp_df))
-
-# Change gdp years to be the same as co2 years
-total_gdp_df$year <- c(
-  "YR1998", "YR1999", "YR2000", "YR2001", "YR2002", "YR2003", "YR2004", "YR2005", "YR2006",
-  "YR2007", "YR2008", "YR2009", "YR2010", "YR2011", "YR2012", "YR2013", "YR2014"
-)
+gdp_long <- gather(gdp_interactive,
+                  key = year, value = GDP, 
+                  "YR1998", "YR1999", "YR2000", "YR2001", "YR2002", "YR2003",
+                  "YR2004", "YR2005", "YR2006", "YR2007",
+                  "YR2008", "YR2009", "YR2010", "YR2011",
+                  "YR2012", "YR2013", "YR2014")
 
 
-# join co2 and gdp data
-both_co2_gdp <- left_join(total_co2_df, total_gdp_df, by = "year")
+gdp_co2_interactive <- left_join(co2_long, gdp_long, by = c("Country.Code", "year"))
 
 # organize columns
-both_co2_gdp <- both_co2_gdp %>%
-  select(year, total_co2_emissions_kt, total_gdp)
+gdp_co2_interactive <- gdp_co2_interactive %>%
+  select(Country.Code, Country.Name, year, co2_emitted, GDP)
 
-# Plot data
-plot_graphic <- ggplot(data = both_co2_gdp) +
-  geom_point(mapping = aes(x = year, y = total_co2_emissions_kt, color = total_gdp)) +
-  scale_color_gradientn(colors = c("red", "green")) +
+
+
+# gdp_co2_interactive_2014 <- gdp_co2_interactive %>% 
+#   filter(year == "YR2014")
+
+# Get regional info (from assignment 3 data folder)
+world_region_info <- read.csv("data/life_expectancy.csv", stringsAsFactors = FALSE)
+
+world_region_info <- world_region_info %>% 
+  select(country, region, income_group)
+
+gdp_co2_interactive <- left_join(gdp_co2_interactive, world_region_info, 
+                                      by = c("Country.Name" = "country"))
+colnames(gdp_co2_interactive)
+View(gdp_co2_interactive)
+
+usa_and_china <- gdp_co2_interactive %>% 
+  filter(Country.Name %in% c("United States", "China"))
+usa_and_china
+interactive_plot <- gdp_co2_interactive %>%
+  plot_ly(
+    x = ~GDP,
+    y = ~co2_emitted,
+    frame = ~year,
+    type = "scatter",
+    mode = "markers",
+    color = ~region,
+    text = ~Country.Name,
+    showlegend = F
+  ) %>%
+  layout(
+    xaxis = list(type = "log"),
+    yaxis = list(type = "log")
+    )
+
+interactive_plot
+
+plot_gdp_by_co2 <- ggplot(data = gdp_co2_interactive) +
+  geom_point(mapping = aes(x = GDP, y = co2_emitted, color = region)) +
+  scale_color_brewer(palette = "Set3") +
   theme(axis.text.x = element_text(angle = 60, hjust = 1)) +
-  labs(title = "Change in CO2 Emissions Over Time in Relation to GDP")
+  labs(title = "Scatter Plot of Country CO2 Emissions and GDP on a Log Scale") +
+  scale_x_log10() +
+  scale_y_log10()
 
-plot_graphic
+plot_gdp_by_co2
 
 
-plot_regional <- plot_ly(
-  both_co2_gdp,
-  x = ~ year, y = ~ total_co2_emissions_kt, type = "scatter",
-  color = ~ total_gdp
-)
 
-plot_regional
 
+# Resources Used:
 # http://eriqande.github.io/rep-res-web/lectures/making-maps-with-R.html
 # https://plot.ly/r/line-and-scatter/
 # https://www.rstudio.com/wp-content/uploads/2015/03/ggplot2-cheatsheet.pdf 
+# https://blog.exploratory.io/filter-data-with-dplyr-76cf5f1a258e 
+# https://plot.ly/r/animations/ 
